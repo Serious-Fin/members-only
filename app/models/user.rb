8bd9a399291ff2f8d9c5e120ba267e48b2805e1d
Serious-Fin <augustasgri@gmail.com>
2023-05-26 22:31:08 +0300

    Complete User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors.add attribute, (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,4 +13,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :password, presence: true, length: {minimum: 5}
+  validates :email, presence: true, uniqueness: true, email: true
 end
