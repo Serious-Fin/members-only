6044570a523aff1cf285ad75e74bb563e148ad30
Serious-Fin <augustasgri@gmail.com>
2023-05-28 22:21:29 +0300

    Complete creation of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,22 @@ class PostsController < ApplicationController
   end
 
   def create
+    #@post = Post.new(title: params[:post][:title], body: params[:post][:body], user_id: current_user.id)
+    @post = Post.new(post_params.merge(user_id: current_user.id))
+
+    if @post.save
+      redirect_to action: "index"
+    else
+      render :new, status: :unprocessable_entity, plain: params
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
